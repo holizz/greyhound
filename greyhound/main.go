11c02cddac9251c254c2671e9698f64ca5ca081e
Tom Adams <tom@holizz.com>
2014-02-04 09:19:29 -0500

    Add -i option for ignoring errors to cli tool

--- greyhound/main.go
+++ greyhound/main.go
@@ -9,13 +9,26 @@ import (
 	"time"
 )
 
+type stringslice []string
+
+func (s *stringslice) String() string {
+	return "a"
+}
+
+func (s *stringslice) Set(in string) (err error) {
+	*s = append(*s, in)
+	return
+}
+
 func main() {
+	ignore := stringslice([]string{})
 	port := flag.Int("p", 3000, "port number to listen on")
 	dir := flag.String("d", ".", "directory to serve")
 	timeout := flag.Duration("t", time.Second * 5, "timeout in milliseconds")
+	flag.Var(&ignore, "i", "ignore errors matching this string")
 	flag.Parse()
 
-	phpHandler, err := greyhound.NewPhpHandler(*dir, *timeout, []string{})
+	phpHandler, err := greyhound.NewPhpHandler(*dir, *timeout, ignore)
 	if err != nil {
 		log.Fatalln(err)
 	}
