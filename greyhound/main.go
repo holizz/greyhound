bed50eff42fc1a62afe56ffaeb877b035a76be77
Tom Adams <tom@holizz.com>
2014-02-05 22:54:19 -0500

    Point out that multiple -i options are allowed

--- greyhound/main.go
+++ greyhound/main.go
@@ -26,7 +26,7 @@ func main() {
 	port := flag.Int("p", 3000, "port number to listen on")
 	dir := flag.String("d", ".", "directory to serve")
 	timeout := flag.Duration("t", time.Second * 5, "timeout in milliseconds")
-	flag.Var(&ignore, "i", "ignore errors matching this string")
+	flag.Var(&ignore, "i", "ignore errors matching this string (multiple allowed)")
 	flag.Parse()
 
 	if len(flag.Args()) > 0 {
