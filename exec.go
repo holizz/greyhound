386f2e2c57154e0999011b8b4567d18e6982a7fd
Tom Adams <tom@dxw.com>
2014-03-27 10:06:21 -0400

    Attempt to get it actually reading the PHP file

--- exec.go
+++ exec.go
@@ -4,32 +4,32 @@ import (
 	"bufio"
 	"errors"
 	"io"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"time"
 )
 
-// https://gist.github.com/tamagokun/3801087
 var routerPhp = `<?php
- 
-$root = $_SERVER['DOCUMENT_ROOT'];
-chdir($root);
-$path = '/'.ltrim(parse_url($_SERVER['REQUEST_URI'])['path'],'/');
-set_include_path(get_include_path().':'.__DIR__);
-if(file_exists($root.$path))
-{
-	if(is_dir($root.$path) && substr($path,strlen($path) - 1, 1) !== '/')
-		$path = rtrim($path,'/').'/index.php';
-	if(strpos($path,'.php') === false) return false;
-	else {
-		chdir(dirname($root.$path));
-		require_once $root.$path;
-	}
-}else include_once 'index.php';
 `
+
+func createRouterPhp() string {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	f.WriteString(routerPhp)
+	return f.Name()
+}
 
 // A low-level command
 // Starts PHP running, waits half a second, returns an error if PHP stopped during that time
 func runPhp(dir, host string, extraArgs []string) (cmd *exec.Cmd, stdout chan string, stderr chan string, errorChan chan error, err error) {
+	tmpFile := createRouterPhp()
+	defer os.Remove(tmpFile)
+
 	args := []string{
 		"-n", // do not read php.ini
 		"-S", host,
@@ -40,16 +40,11 @@ func runPhp(dir, host string, extraArgs []string) (cmd *exec.Cmd, stdout chan st
 		"-d", "upload_max_filesize=1024G",
 		"-d", "post_max_size=1024G",
 		"-d", "max_execution_time=0",
+		tmpFile,
 	}
 	args = append(args, extraArgs...)
 
 	cmd = exec.Command("php", args...)
-
-	// Connect stdin
-	_stdin, err := cmd.StdinPipe()
-	if err != nil {
-		panic(err)
-	}
 
 	// Connect stdout
 	_stdout, err := cmd.StdoutPipe()
@@ -70,8 +65,6 @@ func runPhp(dir, host string, extraArgs []string) (cmd *exec.Cmd, stdout chan st
 
 	go func() {
 		err = cmd.Run()
-		defer _stdin.Close()
-		io.WriteString(_stdin, routerPhp)
 		if err != nil {
 			errorChan <- err
 		} else {
